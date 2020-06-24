70ca6f5944695c6f0cfc4a26768eb66d61ab0dc9
Daniel Ben-Shloosh <danielbenshloosh@gmail.com>
2020-06-24 15:44:01 +0300

    Adding files

---
+++ MonitorEC2AndRDSInstancesStatus.py
@@ -0,0 +1,269 @@
+import json
+import boto3
+import datetime
+import calendar
+from jsondiff import diff
+
+
+def proccessData(newJsonData, table_name,
+                 typeOfCheck):  # This function checks what was the results on the last time check and compare it to the new results
+    try:
+        stringOfAllData = typeOfCheck + "\n\n"  # All the data will added to this variable and finally it will send to email by sns
+
+        lastItem, flagFirstItem = get_last_item_from_DB(table_name)
+        if lastItem["data"] == [] and flagFirstItem:
+            return typeOfCheck + "\nFirst time running - From now the data will be compared to the last time check"
+
+        a, b = json.dumps(lastItem["data"], sort_keys=True), json.dumps(newJsonData["data"],
+                                                                        sort_keys=True)  # Sort jsons - Last json item & new results json  (a=last time json | b=new json)
+        resultFromDiff = json.loads(
+            diff(json.loads(a), json.loads(b), dump=True))  # Diff module checks what is the difference between 2 jsons
+        if resultFromDiff == {}:  # Nothing changed
+            stringOfAllData += "There are the same results from last time"
+        a = json.loads(a)
+        flagValueUpdated = False  # A flag that checks whether there is a change that is an update
+
+        for key in resultFromDiff:
+            try:
+                if key != '$delete' and key != '$insert' and not key.isdigit():
+                    continue  # Skip its the same results
+            except Exception as e:
+                stringOfAllData += "There are the same results from last time"
+                return stringOfAllData
+
+                if key == '$delete' or key == '$insert':
+
+                    for index in range(len(resultFromDiff[key])):  # Pass all over the results
+                        if key == '$delete':
+                            if not flagValueUpdated:  # For adding Event: update once
+                                stringOfAllData += "Event: Removed Instances" + "\n\n"  # Adding event type if its insert or delete
+                                flagValueUpdated = True  # Need to add one time to flag changed
+                            jsonOfChanges = a[resultFromDiff[key][index]]  # Json of changes
+                            if typeOfCheck == 'RDS':
+                                stringOfAllData += "DB Name:" + jsonOfChanges["db_name"] + " | DB Type:" + \
+                                                   jsonOfChanges["db_type"] + " | DB Engine:" + jsonOfChanges[
+                                                       "engine"] + " | DB Region:" + jsonOfChanges["region"] + "\n"
+                            else:  # EC2
+                                stringOfAllData += "Instance Name:" + jsonOfChanges[
+                                    "instance_name"] + " | Instance ID:" + jsonOfChanges[
+                                                       "instance_id"] + " | Instance Status:" + jsonOfChanges[
+                                                       "status"] + " | Instance Region:" + jsonOfChanges[
+                                                       "region"] + "\n"
+
+                        elif key == '$insert':
+                            if not flagValueUpdated:  # For adding Event: update once
+                                stringOfAllData += "Event: New Instances" + "\n\n"  # Adding event type if its insert or delete
+                                flagValueUpdated = True  # Need to add one time to flag changed
+                            jsonOfChanges = resultFromDiff[key][index][1]  # Json of changes
+                            if typeOfCheck == 'RDS':
+                                stringOfAllData += "DB Name:" + jsonOfChanges["db_name"] + " | DB Type:" + \
+                                                   jsonOfChanges["db_type"] + " | DB Engine:" + jsonOfChanges[
+                                                       "engine"] + " | DB Region:" + jsonOfChanges["region"] + "\n"
+                            else:  # EC2
+                                stringOfAllData += "Instance Name:" + jsonOfChanges[
+                                    "instance_name"] + " | Instance ID:" + jsonOfChanges[
+                                                       "instance_id"] + " | Instance Status:" + jsonOfChanges[
+                                                       "status"] + " | Instance Region:" + jsonOfChanges[
+                                                       "region"] + "\n"
+
+                elif key.isdigit():  # If is digit thats mean its update - this is the index of the row that updated
+
+                    if not flagValueUpdated:  # For adding Event: update once
+                        stringOfAllData += "Event: Updated Instances\n\n"
+                        flagValueUpdated = True  # Need to add one time to flag changed
+                    jsonOfChanges = resultFromDiff[key]  # Json of changes
+                    i = 0
+                    # Adding before and after changes
+                    if typeOfCheck == 'RDS':
+                        stringOfAllData += "Before:" + "DB Name:" + a[int(key)]["db_name"] + " | DB Type:" + \
+                                           a[int(key)]["db_type"] + " | DB Engine:" + a[int(key)][
+                                               "engine"] + " | DB Region:" + a[int(key)]["region"] + "\n"
+                        stringOfAllData += "The Changes:"
+                        for keyOfJson in jsonOfChanges:  # For each update
+                            if i > 0:
+                                stringOfAllData += " | "
+                            if keyOfJson == 'db_name':
+                                stringOfAllData += "DB Name:" + jsonOfChanges[keyOfJson]
+                            elif keyOfJson == 'db_type':
+                                stringOfAllData += "DB Type:" + jsonOfChanges[keyOfJson]
+                            elif keyOfJson == 'engine':
+                                stringOfAllData += "DB Engine:" + jsonOfChanges[keyOfJson]
+                            elif keyOfJson == 'region':
+                                stringOfAllData += "DB Region:" + jsonOfChanges[keyOfJson]
+                            i += 1
+                    else:  # EC2.
+                        stringOfAllData += "Before:" + "Instance Name:" + a[int(key)][
+                            "instance_name"] + " | Instance ID:" + a[int(key)]["instance_id"] + " | Instance Status:" + \
+                                           a[int(key)]["status"] + " | Instance Region:" + a[int(key)]["region"] + "\n"
+                        stringOfAllData += "The Changes:"
+                        for keyOfJson in jsonOfChanges:  # For each update
+                            if i > 0:
+                                stringOfAllData += " | "
+                            if keyOfJson == 'instance_name':
+                                stringOfAllData += "Instance Name:" + jsonOfChanges[keyOfJson]
+                            elif keyOfJson == 'instance_id':
+                                stringOfAllData += "Instance ID:" + jsonOfChanges[keyOfJson]
+                            elif keyOfJson == 'status':
+                                stringOfAllData += "Instance Status:" + jsonOfChanges[keyOfJson]
+                            elif keyOfJson == 'region':
+                                stringOfAllData += "Instance Region:" + jsonOfChanges[keyOfJson]
+                            i += 1
+
+                    stringOfAllData += "\n\n"
+                stringOfAllData += "\n"
+
+        return stringOfAllData
+    except Exception as e:
+        print("proccessData:", e)
+        return None
+
+
+def get_last_item_from_DB(table_name):  # This function get table name and return the last item from table by timestamp
+    dynamo = boto3.resource('dynamodb')
+    table = dynamo.Table(table_name)
+    allTable = table.scan()
+    listOfRowsTimestamp = []
+    i = 0
+    for item in allTable["Items"]:  # Get all items
+        i += 1
+        listOfRowsTimestamp.append(int(item["timestamp"]))
+    if i == 0:
+        unixtime_date = datetime.datetime.utcnow()
+        unixtime_now = calendar.timegm(unixtime_date.utctimetuple())
+        insertItem({"timestamp": unixtime_now, "data": []}, table_name)  # First time
+        return [{"timestamp": unixtime_now, "data": []}, True]
+    lastTimestamp = max(listOfRowsTimestamp)  # Check what is the biggest timestamp (Last time check)
+    lastItem = None
+    for item in allTable["Items"]:
+        if int(item["timestamp"]) == lastTimestamp:
+            lastItem = item  # Save the last item to variable
+            break
+    return [lastItem, False]
+
+
+def check_all_regions_RDS_instances():  # This function return json with the data about RDS's status
+    ec2 = boto3.client('ec2')
+    regions = ec2.describe_regions()
+    listToInsert = []
+    unixtime_date = datetime.datetime.utcnow()
+    # Unixtime now
+    unixtime_now = calendar.timegm(unixtime_date.utctimetuple())
+    for region in regions['Regions']:  # Pass all over regions
+        regionName = region['RegionName']
+        client = boto3.client('rds', region_name=regionName)
+        response = client.describe_db_instances()  # Get all RDS instance of region
+        i = 0
+        for item in response["DBInstances"]:
+            i += 1
+            jsonOfData = {"db_name": str(item["DBInstanceIdentifier"]), "db_type": str(item["DBInstanceClass"]),
+                          "engine": str(item["Engine"]), "region": str(regionName)}  # Data of the RDS
+            listToInsert.append(jsonOfData)
+        jsonToInsert = {"timestamp": unixtime_now, "data": listToInsert}
+
+    return jsonToInsert
+
+
+def insertItem(jsonToInsert, table_name):  # This function get data to insert into dynamoDB by table name
+    dynamo = boto3.resource('dynamodb')
+    # Insert the data into the DynamoDB
+    table = dynamo.Table(table_name)
+    table.put_item(
+        TableName=table_name,
+        Item=json.loads(json.dumps(jsonToInsert))
+    )
+
+
+def sendEmailBySNS(message):  # This function send Emails by SNS Service
+    client = boto3.client('sns')
+    response = client.publish(
+        TargetArn='arn:aws:sns:eu-west-1:558860702682:ec2_instances_status',
+        Message=message,
+        MessageStructure='text'
+    )
+
+
+def check_all_regions_ec2_instances():  # This function check all regions EC2 Instances status
+    ec2 = boto3.client('ec2')
+    listOfJsons = []
+    regions = ec2.describe_regions()
+    unixtime_date = datetime.datetime.utcnow()
+    # Unixtime now
+    unixtime_now = calendar.timegm(unixtime_date.utctimetuple())
+    returnMessage = [str("EC2 Instances\n")]
+    for region in regions['Regions']:  # Pass all over regions
+        regionName = region['RegionName']
+        ec2 = boto3.client('ec2', region_name=regionName)
+        instances = ec2.describe_instances()
+        fullListInstances = instances['Reservations']
+        if fullListInstances == []:
+            continue
+        else:
+            rangeOfList = len(fullListInstances)
+            countHowMuchRunning = 0
+            for index in range(rangeOfList):
+                for instance in fullListInstances[index]['Instances']:
+                    instanceRow = ""
+                    instanceName = "No Name"  # For case that EC2 Instance dose'nt has name
+                    if instance['State']['Name'] == 'running' and countHowMuchRunning == 0:
+                        returnMessage.append("\n\n" + str(regionName + ": "))
+
+                    if 'Tags' in instance:
+                        tagsLength = len(instance['Tags'])
+                        for indexTag in range(tagsLength):  # Get The Name
+                            if instance['Tags'][indexTag]["Key"] == 'Name':
+                                if instance['State']['Name'] == 'running':  # For display only running instances
+                                    instanceRow += str("Instance Name:" + instance['Tags'][indexTag]["Value"]) + " | "
+                                instanceName = instance['Tags'][indexTag]["Value"]  # Get Instance name anyway
+
+                    if instance['State']['Name'] == 'running':
+                        instanceRow += str("Instance ID:" + instance['InstanceId']) + " | "
+                        instanceRow += str("Instance Type:" + instance['InstanceType']) + " | "
+                        instanceRow += str("Instance State:" + instance['State']['Name'])
+                        countHowMuchRunning += 1
+                        returnMessage.append(str(instanceRow))
+                        # Json of status to insert into dynamoDB
+
+                    # Data of the EC2 to insert into dynamoDB
+                    jsonOfData = {"instance_name": str(instanceName), "instance_id": str(instance["InstanceId"]),
+                                  "status": str(instance['State']['Name']), "region": str(regionName)}
+
+                    listOfJsons.append(jsonOfData)
+
+            if countHowMuchRunning == 0:
+                continue  # If there are no running instances
+            else:
+                returnMessage.append("\n\n" + str("There Are " + str(
+                    countHowMuchRunning) + " Running Instances On This Region (" + regionName + ")"))  # Only if there are running instances
+    jsonToInsert = {"timestamp": unixtime_now, "data": listOfJsons}
+    listToReturn = [jsonToInsert, "\n".join(returnMessage)]
+    return (listToReturn)
+
+
+def lambda_handler(event, context):
+    try:
+        # List of json of data & string to send by SNS
+        jsonOfInstancesStatus, stringOfCurrentInstancesStatus = check_all_regions_ec2_instances()
+        jsonOfRDSStatus = check_all_regions_RDS_instances()
+        stringEC2ToSendToSNS = stringOfCurrentInstancesStatus + "\n\n" + proccessData(jsonOfInstancesStatus, "EC2Check",
+                                                                                      "EC2")
+        stringRDSToSendToSNS = "\n\n\n" + proccessData(jsonOfRDSStatus, "RdsCheck", "RDS")
+
+        # Send emails by SNS
+        # Add account name
+        accountName = "AccountName"
+        sendEmailBySNS("Account :" + accountName + "\n\n" + stringEC2ToSendToSNS + stringRDSToSendToSNS)
+
+        # Update dynamoDB with new data
+        insertItem(jsonOfRDSStatus, "RdsCheck")  # Update the table with the new results
+        insertItem(jsonOfInstancesStatus, "EC2Check")  # Update the table with the new results
+        print("Process Success")
+        return {
+            'statusCode': 200,
+            'body': 'success'
+        }
+    except Exception as e:
+        print("Lambda handler:", e)
+        return {
+            'statusCode': 500,
+            'body': 'failed'
+        }
---
+++ README.md
@@ -0,0 +1,149 @@
+<a ><img src="https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png" title="FVCproductions" alt="FVCproductions"></a>
+<!-- [![FVCproductions](https://avatars1.githubusercontent.com/u/4284691?v=3&s=200)](http://fvcproductions.com) -->
+
+# EC2 and RDS Instances status monitor by Lambda
+
+
+
+## Description
+
+Lambda function to monitor EC2 and RDS instances information
+
+- The function goes over all regions that exist in the AWS account
+
+- In each region goes over all EC2 and RDS instances and checks what the statuses of each of the instances are
+
+- While further information is checked from dynamoDB of what the changes have been since the last information test done
+ 
+- At the end of the tests function will send Email with the information via AWS SNS service 
+
+- Now the new instances information will be stored in dynamoDB 
+
+- All of which can be run using the CloudWatch service as scheduled
+
+### Clone
+
+- Clone this repo to your local machine using `git clone https://github.com/fvcproductions/SOMEREPO`
+
+##Services that this Lambda use
+
+> Lambda
+
+> IAM
+
+> CloudWatch
+
+> DynamoDB
+
+> SNS
+
+
+
+## Start working
+> You should create all those services on the same region
+
+**IAM roles**
+> Add roles for allow Lambda function use services
+- Go to roles tab in IAM service at AWS
+- Click on `Create role`
+- Choose Lambda on `Choose a use case` and Click on `Next Permissions`
+- Search and check | `AmazonEC2FullAccess` | `AmazonSNSFullAccess` | `AmazonRDSReadOnlyAccess` |  `AmazonDynamoDBFullAccess` | 
+- Click on `Next`, Fill role name and click on `Create role`
+
+**SNS**
+> Create SNS topic and subscription for send Email with the information
+- Go to topics tab in SNS service at AWS
+- Click on `Create topic`
+- Fill topic name
+- Fill display name - This is the title on the Email that will sent
+- Click on `Create topic`
+- Click on the topic that you created before
+- Click on `Create subscription`
+- Choose on Protocol `Email`
+- Fill on Endpoint the Email that you want to use
+- Click on `Create subscription`
+- After that you need to accept this subscription on your mail box
+
+**DynamoDB**
+> Store the previous results on table and retrieve those results on comparing data
+- Go to tables tab in dynamoDB service at AWS
+- Click on `Create table`
+- Fill Table name with `EC2Check` (Must be this name)
+- Fill Primary key with `timestamp` (Must be this key) 
+- Choose on Primary key type `Number`
+- Click on `Create`
+- Repeat on this section one more time and fill table name with `RdsCheck`  
+- In the end you should have 2 table - `EC2Check` & `RdsCheck`  
+
+
+
+
+**Lambda**
+> Run a Python script that executes the whole process
+
+*Now you need to add layers of python modules*
+
+- Go to Layers tab in Lambda service at AWS
+- Click on `Create layer`
+- Fill Name with `datatime` 
+- Click on `Upload`
+- Choose `datatime.zip` that you've cloned from this repository
+- Choose on Compatible runtimes `Python 3.6`
+- Click on `Create`
+- Repeat on this section one more time and choose `jsondiff.zip` and fill name `jsondiff`
+
+
+
+
+
+*Now you got the python modules that you needs - you can create Lambda function*
+
+- Go to Functions tab in Lambda service at AWS
+- Click on `Create function`
+- Fill Function name
+- Choose on runtime `Python 3.6`
+- Choose on Permissions `Use an existing role` 
+- Choose on Existing role the role that you created on the IAM section 
+- Click on `Create function`
+
+
+*Now you created Lambda function - you can configure this function*
+
+- Click on `Layers`
+- Click on `Add a layer`
+- Add `jsondiff` & `datatime` 
+- Click on `Edit` on Basic settings
+- Increment Memory to `2560MB` as much as the Memory is low the function run more slowly
+- Change Timeout to 2 min (if Memory is under 2560MB increment the time)
+
+*Now you configured Lambda function - you can copy the code into Function code*
+
+- Copy code from `MonitorEC2AndRDSInstancesStatus.py` and paste in Function code 
+- Add on `sendEmailBySNS` function to `TargetArn` variable the arn of the SNS topic that you've created before
+- Add on `lambda_handler` function to `account` variable the right account name
+- Click on `Save`
+- Now you can test your function by click on `Test`
+- You should receive Email with the results from the function
+
+
+
+**CloudWatch**
+> Run the Lambda code as scheduled
+- Go to Rules tab in CloudWatch service at AWS
+- Click on `Create rule`
+- Choose on Event Source `Schedule`
+- Fill on Fixed rate of the number that you want and choose `Minutes | Hours | Days`
+- Click on `Add target` at Targets
+- Choose `Lambda function`
+- Choose the function that you created before
+- Click on `Configure details`
+- Fill Name 
+- Click on `Create rule`
+
+
+
+
+
+
+## License
+- Copyright 2020 © Nandi
